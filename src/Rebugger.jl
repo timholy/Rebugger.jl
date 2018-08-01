ffb564f6f40a6b27a867279a5f0b26ed51d09295
Tim Holy <tim.holy@gmail.com>
2018-08-01 14:04:52 -0500

    Implement the basic UI

--- src/Rebugger.jl
+++ src/Rebugger.jl
@@ -1,60 +1,17 @@
 module Rebugger
 
-using REPL
+using REPL, Random
 using REPL.LineEdit
 using Revise
 using Revise: ExLike, get_signature, funcdef_body, get_def
 
-# Organization:
-
-## Code modification
-## - methods: capture var values and names on entry to a specified slot, optionally throw error. Execute, then restore the original method.
-## - capture vars at buffer position, call @which, throw error. (Here there is no method to repair.)
-
-## Populate REPL with let block of method source
-
-
-
-## Old:
-## F11: step into expr at point (^[[23~)  # Note: on konsole, F11 means fullscreen. Turn off in Settings->Configure Shortcuts
-## Shift-F11: step out (^[[23;2~)
-## Ctrl-F11: step down stacktrace (avoids the need to place point) (^[[23;5~)
-## F5: run command (not a point, from anywhere on REPL input line), collect stacktrace, and enter at end of trace (^[[15~)
-## Shift-F5: clear the rebug stack? (^[[15;2~)  It's not obvious we need this; F5 should clear any existing stack & error stack.
-## "\em" (meta-m): populate Main with arguments to current method
-## X "\es" (meta-s): show current "stacktrace" (perhaps better, do this every time we navigate up or down. This might need separate modes depending on whether we are rebugging an error.)
-## "\eS" (meta-S): save version at REPL to file? (a little dangerous, perhaps make it configurable as to whether this is on)
-
-## Logic:
-## - Maintain a stack of locations (Methods), varnames, and varvals. This makes step into & step out straightforward.
-## - For entering at the end of the trace, do we have to capture all prior states? inlined methods could make this tricky.
-##   consequently we might need an "unknown" status for varnames and varvals.
-##   Once you've captured the stacktrace, it seems that one could rewrite each method to save its
-##   variables (once? or better every time?) to a specific slot in the list.
-## - Could be a little tricky handling both a pure stack (pure stepping) and a list-based approach (debugging an error).
-##   We need storage of "the error stack" separate from "current stack". In the display of the stacktrace, distinguish
-##   things that intersect with the error stack
-
-## Interface/usability questions:
-## - What happens if user just hits enter on a REPL line? That should not clear the stack.
-## - Hitting the up-arrow should repeat the line. Should step-into insert in the history even if
-##   it wasn't executed? (I think yes.)
-## - Suppose a user starts stepping, then clears the input line, writes some more code, and
-##   steps into something else. How to detect that this should extend the stack rather than
-##   reset it? What if you go back manually (via REPL history) to an earlier entry?
-##   Perhaps we should block these from the history and require that the user retrieves them
-##   manually.
-
-## OK, so a plain F11 resets the stack. However, it also sets a flag called `stepping`. If
-## stepping is true the stack gets extended by the next F11.
-## Hitting enter on the REPL sets `stepping` false (do this from the repl backend). F5 sets it to true again.
-
 const VarnameType = Tuple{Vararg{Symbol}}
 const stack       = Tuple{Method,VarnameType,Any}[]
+const stackid     = Ref(randstring(8))
+const stackcmd    = String[]
 const error_stack = Tuple{Method,VarnameType,Any}[]
 const stashed     = Ref{Any}(nothing)
-
-const stepping = Ref(false)
+const msgs        = []   # for debugging
 
 struct StopException <: Exception end
 
@@ -102,12 +59,12 @@ function capture_stacktrace(mod::Module, command::Expr)
     capture_stacktrace!(reverse!(calledmethods)) do
         eval_noinfo(mod, command)
     end
-    # # Set up stack and error_stack
-    # populate_replinput(lastitemonstack)  # move this elsewhere
+    return nothing
 end
 capture_stacktrace(command::Expr) = capture_stacktrace(Main, command)
 
 function capture_stacktrace!(f::Function, calledmethods::Vector)
+    index = length(calledmethods)
     if isempty(calledmethods)
         # We've finished modifying all the methods, time to run the command
         try
@@ -120,9 +77,9 @@ function capture_stacktrace!(f::Function, calledmethods::Vector)
     method = calledmethods[end]
     def = get_def(method)
     if def != nothing
-        # Now modify `def` to insert into slot `length(calledmethods)` (don't throw an error)
+        # Now modify `def` to insert into slot `index` (don't throw an error)
         # and eval it in the appropriate module
-        method_capture_from_callee(method, def, length(calledmethods); trunc=false)
+        method_capture_from_callee(method, def, index; trunc=false)
     end
     # Recurse up the stack until we get to the top...
     pop!(calledmethods)
@@ -167,6 +124,11 @@ and [`stepin!`](@ref) which puts these two together.
 function capture_from_caller!(s)  # for testing, needs to work on a normal IO object
     start = position(s)
     callstring = LineEdit.content(s)
+    index, hasid = get_stack_index(callstring)
+    index += 1
+    if !hasid
+        stackid[] = randstring(8)
+    end
     expr, stop = Meta.parse(callstring, start+1; raise=false)
     (isa(expr, Expr) && expr.head == :call) || throw(Meta.ParseError("Point must be at a call expression, got $expr"))
     fname, args = expr.args[1], expr.args[2:end]
@@ -190,11 +152,11 @@ function capture_from_caller!(s)  # for testing, needs to work on a normal IO ob
     regexscoped   = r"(?<!\.)Rebugger\.StopException"
     capturestr = replace(capturestr, regexscoped=>(s->"Main."*s))
     LineEdit.edit_splice!(s, start=>stop-1, capturestr*"\n")
-    return callstring, LineEdit.content(s)
+    return callstring, LineEdit.content(s), index
 end
 
 """
-    stepin!(s, index=length(Rebugger.stack)+1)
+    stepin!(s)
 
 Given a buffer `s` representing a string and "point" (the seek position) set at a call expression,
 replace the contents of the buffer with a `let` expression that wraps the *body* of the callee.
@@ -223,10 +185,10 @@ set when you called `fcomplex(x)` in `s` above.
 This line can be edited and `eval`ed at the REPL to analyze or improve `fcomplex`,
 or can be used for further `stepin!` calls.
 """
-function stepin!(s, index=length(stack)+1)
+function stepin!(s)
     @assert(stashed[] == nothing)
     ## Stage 1 is "stashing". We do this simply to determine which method is called.
-    callstring, stashstring = capture_from_caller!(s)
+    callstring, stashstring, index = capture_from_caller!(s)
     callexpr, stashexpr = Meta.parse(callstring), Meta.parse(stashstring)
     try
         Core.eval(Main, stashexpr)
@@ -255,12 +217,12 @@ function stepin!(s, index=length(stack)+1)
     catch err
         err isa StopException || rethrow(err)
     end
+    resizestack(index)
     ## Stage 4: clean up and insert the new method body into the REPL
     # Restore the original method
     eval_noinfo(method.module, def)
     # Dump the method body to the REPL
     generate_let_command(s, index)
-    stepping[] = true
     return nothing
 
     @label writeback
@@ -268,6 +230,42 @@ function stepin!(s, index=length(stack)+1)
     LineEdit.edit_insert(s, callstring)
     return nothing
 end
+
+function stepup!(s)
+    callstring = LineEdit.content(s)
+    index, hasid = get_stack_index(callstring)
+    if hasid && index > 1
+        letcommand = stackcmd[index-1]
+        LineEdit.edit_clear(s)
+        LineEdit.edit_insert(s, letcommand)
+    end
+    return nothing
+end
+
+function stepdown!(s)
+    callstring = LineEdit.content(s)
+    index, hasid = get_stack_index(callstring)
+    if hasid && index < length(stack)
+        letcommand = stackcmd[index+1]
+        LineEdit.edit_clear(s)
+        LineEdit.edit_insert(s, letcommand)
+    end
+    return nothing
+end
+
+function capture_stacktrace(s)
+    resizestack(0)
+    cmdstring = LineEdit.content(s)
+    expr = Meta.parse(cmdstring)
+    capture_stacktrace(expr)
+    for index = 1:length(stack)
+        stackcmd[index] = generate_let_command(index)
+    end
+    LineEdit.edit_clear(s)
+    LineEdit.edit_insert(s, stackcmd[end])
+    return nothing
+end
+
 
 ### Shared methods
 
@@ -312,41 +310,44 @@ function method_capture_from_callee(method, def, index; trunc::Bool=false)
     end
     capture_function = Expr(:function, sig, capture_body)
     if index > length(stack)
-        resize!(stack, index)
+        resizestack(index)
     end
     mod = method.module
     eval_noinfo(mod, capture_function)
 end
 
-function generate_let_command(s, index)
+function generate_let_command(index)
     method, varnames, varvals = stack[index]
     argstring = '(' * join(varnames, ", ") * ')'
     body = convert(Expr, Revise.striplines!(deepcopy(funcdef_body(get_def(method)))))
     letcommand = """
-        @eval $(method.module) let $argstring = Main.Rebugger.stack[$index][3]
+        @eval $(method.module) let $argstring = Main.Rebugger.stack[$index][3]  # stackid $(stackid[])
         $body
         end"""
+    end
+
+function generate_let_command(s, index)
+    letcommand = generate_let_command(index)
+    stackcmd[index] = letcommand
     LineEdit.edit_clear(s)
     LineEdit.edit_insert(s, letcommand)
 end
 
 ### Key bindings
 
-## REPL commands:
-## F11 (^[[23~): step into expr at point   # Note: on konsole, F11 means fullscreen.
-## Shift-F11 (^[[23;2~): step out
-## Ctrl-F11 (^[[23;5~): step down stacktrace (avoids the need to place point)
-## F5 (^[[15~): reinsert the last rebugger command. This allows extension of the stack.
-## F9 (^[[20~): capture a stacktrace and start at the bottom
+## REPL commands TODO:
 ## "\em" (meta-m): create REPL line that populates Main with arguments to current method
 ## "\eS" (meta-S): save version at REPL to file? (a little dangerous, perhaps make it configurable as to whether this is on)
 
 const rebuggerkeys = Dict{Any,Any}(
     # F11. Note for `konsole` (KDE) users, F11 means "fullscreen". Turn off in Settings->Configure Shortcuts
-    "\e[23~" => (s, o...) -> begin
-        stepping[] || empty!(stack)
-        stepin!(s)
-    end,
+    "\e[23~"   => (s, o...) -> stepin!(s),
+    # Shift-F11
+    "\e[23;2~" => (s, o...) -> stepup!(s),
+    # Ctrl-F11
+    "\e[23;5~" => (s, o...) -> stepdown!(s),
+    # F5
+    "\e[15~"   => (s, o...) -> capture_stacktrace(s),
 )
 
 function customize_keys(repl)
@@ -407,6 +408,27 @@ function signature_names(sigex::ExLike)
     end
 
     return sigex.args[1], tuple(argname.(sigex.args[offset+1:end])...), kwnames, parameternames
+end
+
+function get_stack_index(callstring)
+    index = length(stack)
+    hasid = occursin("# stackid $(stackid[])", callstring)
+    if hasid
+        iend1 = findfirst(isequal('\n'), callstring)
+        iend1 == nothing ? length(callstring) : iend1
+        firstline = callstring[1:iend1]
+        mindex = match(r"Main.Rebugger.stack\[(\d+)\]", firstline)
+        index = parse(Int, mindex.captures[1])
+    else
+        index = 0
+    end
+    push!(msgs, (index, hasid))
+    return index, hasid
+end
+
+function resizestack(index)
+    resize!(stack, index)
+    resize!(stackcmd, index)
 end
 
 safe_deepcopy(a, args...) = (_deepcopy(a), safe_deepcopy(args...)...)
